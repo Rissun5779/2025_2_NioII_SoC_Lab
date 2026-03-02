222924e3f22547aaceeb7663686f21101f775efa
rissun5779 <rissun57@gmail.com>
2026-03-02 16:47:16 +0800

    Finish Lab4 Basic Seg7 Display

---
+++ Lab4_DE10/software/Lab4/obj/default/main.d
@@ -0,0 +1,11 @@
+obj/default/main.o: main.c ../Lab4_bsp//HAL/inc/io.h \
+ ../Lab4_bsp//HAL/inc/alt_types.h ../Lab4_bsp/system.h \
+ ../Lab4_bsp/linker.h
+
+../Lab4_bsp//HAL/inc/io.h:
+
+../Lab4_bsp//HAL/inc/alt_types.h:
+
+../Lab4_bsp/system.h:
+
+../Lab4_bsp/linker.h:
